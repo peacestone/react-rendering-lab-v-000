cb26d05167b53e9a25801a2e353b175a29cbfa2d
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-07-10 14:20:26 -0400

    Done.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,18 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.rating > nextProps.rating) {
+      this.setState({increasing: false, decreasing: true})
+    }
+    else {
+      this.setState({increasing: true, decreasing: false})
+    }
+    if (this.props.rating === nextProps.rating) {
+      this.setState({increasing: false, decreasing: false})
+    }
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
